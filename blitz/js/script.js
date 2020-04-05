e88d65de8b7e7f939f0bdea464971bfab3cb9d65
Artem Gladkov <art123456789063@gmail.com>
2020-04-05 22:41:29 +0300

    Изменен header, добавлен выпадающий siadebar-r

--- blitz/js/script.js
+++ blitz/js/script.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
         $('.header__burger, .header-nav').toggleClass('active');
         $('body').toggleClass('lock');
     })
+    $('.header__sidebar-r').click(function(event){
+        $('.sidebar-r_mobile, .header__sidebar-r').toggleClass('active');
+    })
 })
