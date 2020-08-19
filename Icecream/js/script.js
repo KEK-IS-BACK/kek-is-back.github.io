7a4cbc8fd586d3ba0b64464b69e996c621ad4f49
unknown <art123456789063@gmail.com>
2020-08-19 10:36:05 +0300

    delete Icecream

--- Icecream/js/script.js
+++
@@ -1,9 +0,0 @@
-let sliderFor = document.querySelector('.slider-for'),
-    sliderNav = document.querySelectorAll('.slider-nav-item')
-
-sliderNav.forEach(function(e){
-    e.addEventListener('click', function(){
-        let item = this.innerHTML
-        sliderFor.innerHTML = item
-    })
-})
